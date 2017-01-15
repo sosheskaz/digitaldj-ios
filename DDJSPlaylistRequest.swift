c7b01c7468174b7e78911898f0b049d72a95db8a
Eric Miller <millere@msoe.edu>
2017-01-14 18:26:26 -0600

    Figured out some more of the spotify framework and carthage dependencies, added command structure

--- DDJSPlaylistRequest.swift
+++ DDJSPlaylistRequest.swift
@@ -6,29 +6,57 @@
 //  Copyright © 2016 msoe. All rights reserved.
 //
 
+import Alamofire
 import Foundation
 import WebKit
 
-let defaultEndpoint: String = "https://DigitalDJ-1142864711.us-west-2.elb.amazonaws.com"
+let isSecure = false
+let prtcl = isSecure ? "https" : "http"
+let defaultEndpoint: String = "\(prtcl)://digitaldj.us-west-2.elasticbeanstalk.com/api/v1/generateplaylist"
 
 class DDJSPlaylistRequest {
     var endpoint: String = defaultEndpoint
     var endpointUrl: URL
     var session = URLSession(configuration: .default)
-    var jsonDict: Any
+    var jsonDict: [String: AnyObject]
     
     convenience init(endpoint: String = defaultEndpoint, oauthTokens: Array<String>) {
         self.init()
         self.endpoint = endpoint
         self.endpointUrl = URL(string: self.endpoint)!
         self.jsonDict = [
-            "authTokens": oauthTokens
+            "authTokens": oauthTokens as AnyObject
         ]
     }
     
     init() {
         self.endpointUrl = URL(string: self.endpoint)!
-        self.jsonDict = []
+        self.jsonDict = [:]
+    }
+    
+    func doRequestAF(callback: @escaping (Int?, Data?) -> Void) -> Void {
+        let headers: HTTPHeaders = [
+            "Content-Type": "application/json"
+        ]
+        
+        print("req start")
+        do {
+        print(String(data: try JSONSerialization.data(withJSONObject: self.jsonDict, options: []), encoding: String.Encoding.utf8) ?? "FAIL")
+        } catch {
+            print("DOUBLE FAIL")
+        }
+        
+        let queue = DispatchQueue(label: "com.ddj.alamofire-queue", qos: .utility, attributes: [.concurrent])
+        let req = Alamofire.request(endpoint, method: .post, parameters: self.jsonDict, encoding: JSONEncoding.default, headers: headers).validate().response(queue: queue, completionHandler: { response in
+            print("HIT")
+            
+            print(String(data: response.data!, encoding: String.Encoding.utf8))
+            callback(response.response?.statusCode, response.data)
+            
+            print(response.data)
+        })
+        
+        req.resume()
     }
     
     func doRequest(callback: @escaping (Any) -> Void) -> Void {
@@ -38,12 +66,14 @@ class DDJSPlaylistRequest {
             request.httpMethod = "POST"
             do {
                 let json = try JSONSerialization.data(withJSONObject: self.jsonDict)
-                let postString = "\"" + String(data: json, encoding: String.Encoding.utf8)! + "\""
-                request.httpBody = postString.data(using: String.Encoding.utf8)
+                var bodyString = "\"\(String(data: json, encoding: String.Encoding.utf8)!.replacingOccurrences(of: "\"", with: "'"))\""
+                print(bodyString)
+                request.allHTTPHeaderFields?["Content-Type"] = "application/text"
+                request.httpBody = bodyString.data(using: .utf8)
             } catch {
                 print("uh oh spaggettios")
             }
-            
+
             let task = self.session.dataTask(with: request, completionHandler: { data, response, error in
                 print("Doing URLSession DataTask")
                 print(data == nil)
---
+++ DDJiOS/HostClientCommand.swift
@@ -0,0 +1,13 @@
+//
+//  HostClientCommand.swift
+//  DDJiOS
+//
+//  Created by Eric Miller on 1/14/17.
+//  Copyright © 2017 msoe. All rights reserved.
+//
+
+import Foundation
+
+protocol HostClientCommand: Command {
+    
+}
