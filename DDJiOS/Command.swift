902d1482b826b6ca8c1ef985c77b173325c595ad
Eric Miller <millere@msoe.edu>
2017-01-14 19:46:45 -0600

    Building command execution framework, added dependency and dependency resolution script.

--- DDJiOS/Command.swift
+++ DDJiOS/Command.swift
@@ -7,9 +7,25 @@
 //
 
 import Foundation
+import SwiftSocket
 
 let commandLabel: String = "command"
 protocol Command {
     static var command: String {get}
     var json: Data? {get} // Failable
+}
+
+extension Command {
+    func execute(_ address: String, port: Int32) {
+        let client = TCPClient(address: address, port: port)
+        switch client.connect(timeout: 10) {
+        case .success:
+            // Connection successful 🎉
+            client.send(data: json!)
+            break
+        case .failure(let error):
+            // 💩
+            break
+        }
+    }
 }
