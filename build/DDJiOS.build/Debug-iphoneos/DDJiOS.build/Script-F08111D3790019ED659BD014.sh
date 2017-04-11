2d931cb163fff093cc3abddf9ed38def540f11d1
Eric Miller <millere@msoe.edu>
2017-04-11 18:45:10 -0500

    More pods fixes

---
+++ build/DDJiOS.build/Debug-iphoneos/DDJiOS.build/Script-F08111D3790019ED659BD014.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+"${SRCROOT}/Pods/Target Support Files/Pods-DDJiOS/Pods-DDJiOS-resources.sh"
+
--- build/Pods.build/Release-iphoneos/Alamofire.build/Objects-normal/arm64/Alamofire-dummy.d
+++ build/Pods.build/Release-iphoneos/Alamofire.build/Objects-normal/arm64/Alamofire-dummy.d
@@ -1,3 +1,4 @@
 dependencies: \
   /Users/ericmiller/Documents/MSOE/Senior\ Design/ddjios/Pods/Target\ Support\ Files/Alamofire/Alamofire-dummy.m \
+  /Users/ericmiller/Documents/MSOE/Senior\ Design/ddjios/build/Pods.build/Release-iphoneos/Alamofire.build/module.modulemap \
   /Users/ericmiller/Documents/MSOE/Senior\ Design/ddjios/Pods/Target\ Support\ Files/Alamofire/Alamofire-prefix.pch
